0f6e7b9ce7b564e80e2c688858b80892dc491e54
q <vcapkov167@gmail.com>
2025-06-07 12:33:01 +0300

    refactoring

--- Lab5/src/test/NavigationSystemTest.java
+++ Lab5/src/test/NavigationSystemTest.java
@@ -7,6 +7,7 @@ import navigation.NavigationSystem;
 import navigation.Map;
 import navigation.SchematicMap;
 import navigation.RouteStrategy;
+import navigation.SatelliteMap;
 import navigation.FastestRouteStrategy;
 import navigation.CheapestRouteStrategy;
 import navigation.ShortestRouteStrategy;
@@ -17,9 +18,15 @@ import navigation.Bicycle;
 class NavigationSystemTest {
 
 	@Test
-	void testNavigationSystemTest() {
-		NavigationSystem navigationSystem = new NavigationSystem(new SchematicMap(), new FastestRoute(), new Bike());
-		assertDoesNotThrow(navigationSystem::navigate);
+	void testNavigationSystemFastest() {
+		NavigationSystem navigationSystem1 = new NavigationSystem(new SchematicMap(), new FastestRouteStrategy(), new Bicycle());
+		assertDoesNotThrow(navigationSystem1::navigate);
+	}
+	
+	@Test
+	void testNavigationSystemCheapest() {
+		NavigationSystem navigationSystem2 = new NavigationSystem(new SatelliteMap(), new CheapestRouteStrategy(), new Car());
+		assertDoesNotThrow(navigationSystem2::navigate);
 	}
 
 }
